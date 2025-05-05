ae413f3e508e86212cc1b9acd210e73c6324acdd
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2025-05-04 21:28:23 -0300

    ruteo carrito final

--- routers/carrito.router.js
+++ routers/carrito.router.js
@@ -3,7 +3,7 @@ const routerCarrito = express.Router()
 import controller from '../controllers/carrito.controller.js'
 
 
-// ! GetAll
+
 
 // ! Hacer un post (Create) para que se efectue la compra.
 routerCarrito.post('/', controller.guardarCarrito)
